e02e6e99d723466fbe823e088f555eff74ef6399
Siva shanmuganathan <sivashanmuganathan2306@gmail.com>
2024-06-01 21:49:32 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/Sivashanmuganathan/', // The base URL of your repository
+  build: {
+    rollupOptions: {
+      external: [path.resolve(__dirname, 'pages/main.jsx')]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 });
