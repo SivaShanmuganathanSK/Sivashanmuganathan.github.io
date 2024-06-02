80c45000cd99162bc1c1036e38a5c315ad46c401
Siva shanmuganathan <sivashanmuganathan2306@gmail.com>
2024-06-02 16:04:17 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../App.jsx'
+import App from './App.jsx'
 import './index.css'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Portfolio from './Portfolio/Portfolio.jsx';
-import Blogs from './Blogs/Blogs.jsx';
-import Resume from './Resume/Resume.jsx';
-import About from './About/About.jsx';
-import Contact from './Contact/Contact.jsx';
+import Portfolio from './pages/Portfolio/Portfolio.jsx';
+import Blogs from './pages/Blogs/Blogs.jsx';
+import Resume from './pages/Resume/Resume.jsx';
+import About from './pages/About/About.jsx';
+import Contact from './pages/Contact/Contact.jsx';
 
 const router = createBrowserRouter([
   {
